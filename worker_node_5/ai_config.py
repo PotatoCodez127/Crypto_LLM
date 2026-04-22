4ee084b046239e820618d9a6175cf22300790451
jeandre <jeandre.c127@gmail.com>
2026-04-22 16:14:58 +0200

    Auto-experiment: Set max_depth=12, learning_rate=0.2, n_estimators=...

--- worker_node_5/ai_config.py
+++ worker_node_5/ai_config.py
@@ -9,9 +9,9 @@ FEATURES = [
 ]
 
 MODEL_PARAMS = {
-    'max_depth': 20,
-    'learning_rate': 0.05,
-    'n_estimators': 300,
+    'max_depth': 12,
+    'learning_rate': 0.2,
+    'n_estimators': 150,
     'random_state': 42,
     'n_jobs': -1
 }

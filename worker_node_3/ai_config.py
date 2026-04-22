ab0db1449effb9220dd9d861590869d3bf7ab0ba
jeandre <jeandre.c127@gmail.com>
2026-04-22 15:39:04 +0200

    Auto-experiment: Set hyperparameters to max_depth=20, learning_rate...

--- worker_node_3/ai_config.py
+++ worker_node_3/ai_config.py
@@ -9,8 +9,8 @@ FEATURES = [
 ]
 
 MODEL_PARAMS = {
-    'max_depth': 3,
-    'learning_rate': 0.05,
+    'max_depth': 20,
+    'learning_rate': 0.5,
     'n_estimators': 100,
     'random_state': 42,
     'n_jobs': -1

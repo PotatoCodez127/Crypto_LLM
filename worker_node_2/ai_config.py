34cb32e294a6397887562d27e782b904012c874c
jeandre <jeandre.c127@gmail.com>
2026-04-22 17:40:25 +0200

    Auto-experiment: Use all V2 features, set max_depth=8, learning_rat...

--- worker_node_2/ai_config.py
+++ worker_node_2/ai_config.py
@@ -9,9 +9,9 @@ FEATURES = [
 ]
 
 MODEL_PARAMS = {
-    'max_depth': 30,
-    'learning_rate': 0.05,
-    'n_estimators': 200,
+    'max_depth': 8,
+    'learning_rate': 0.1,
+    'n_estimators': 100,
     'random_state': 42,
     'n_jobs': -1
 }

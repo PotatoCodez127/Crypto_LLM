630bc3356819b7074ff4658cbbad2031b528e4d2
jeandre <jeandre.c127@gmail.com>
2026-04-22 16:14:16 +0200

    Auto-experiment: Set max_depth to 10, learning_rate to 0.03, n_esti...

--- worker_node_1/ai_config.py
+++ worker_node_1/ai_config.py
@@ -3,13 +3,15 @@
 
 FEATURES = [
     'cvd_trend', 
-    'close_zscore_50'
+    'atr_14',
+    'close_zscore_50',
+    'volume_zscore_24'
 ]
 
 MODEL_PARAMS = {
     'max_depth': 10,
-    'learning_rate': 0.1,
-    'n_estimators': 100,
+    'learning_rate': 0.03,
+    'n_estimators': 150,
     'random_state': 42,
     'n_jobs': -1
 }

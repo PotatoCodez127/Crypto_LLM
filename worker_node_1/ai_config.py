55c10ebb399f6c471ed6f5f0391e0374deab753f
jeandre <jeandre.c127@gmail.com>
2026-04-23 13:42:53 +0200

    Auto-experiment: FEATURES=['cvd_trend', 'atr_14', 'close_zscore_50'...

--- worker_node_1/ai_config.py
+++ worker_node_1/ai_config.py
@@ -1,4 +1,4 @@
-FEATURES=['rsi_14', 'macd_line', 'atr_14', 'close_zscore_50']
-TARGET_LOOKAHEAD=3
+FEATURES=['cvd_trend', 'atr_14', 'close_zscore_50', 'volume_zscore_24', 'rsi_14', 'macd_line', 'bb_lower', 'bb_upper']
+TARGET_LOOKAHEAD=1
 THRESHOLD_PERCENTILE=70
-MODEL_PARAMS={'max_depth': 8, 'learning_rate': 0.1, 'n_estimators': 200}
+MODEL_PARAMS={'max_depth': 6, 'learning_rate': 0.1, 'n_estimators': 100}

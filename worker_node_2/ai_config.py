9a26b639b938f0390c3a0229efc0326bba63327e
jeandre <jeandre.c127@gmail.com>
2026-04-23 14:11:08 +0200

    clean

--- worker_node_2/ai_config.py
+++
@@ -1,4 +0,0 @@
-FEATURES=['cvd_trend', 'atr_14', 'close_zscore_50', 'volume_zscore_24', 'rsi_14', 'macd_line', 'bb_lower', 'bb_upper']
-TARGET_LOOKAHEAD=1
-THRESHOLD_PERCENTILE=70
-MODEL_PARAMS={'max_depth': 3, 'learning_rate': 0.01, 'n_estimators': 500}

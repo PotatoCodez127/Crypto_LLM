8a7c764296e9e4791ebfd7ec74df88df323b7832
jeandre <jeandre.c127@gmail.com>
2026-04-23 13:26:43 +0200

    Auto-experiment: This setup will generate a high number of trades, ...

--- worker_node_2/ai_config.py
+++ worker_node_2/ai_config.py
@@ -1,4 +1,4 @@
-FEATURES=['cvd_trend', 'rsi_14', 'macd_line', 'close_zscore_50', 'volume_zscore_24']
+FEATURES=['cvd_trend', 'atr_14', 'close_zscore_50', 'volume_zscore_24', 'rsi_14', 'macd_line', 'bb_lower', 'bb_upper']
 TARGET_LOOKAHEAD=1
 THRESHOLD_PERCENTILE=70
-MODEL_PARAMS={'max_depth': 4, 'learning_rate': 0.15, 'n_estimators': 80}
+MODEL_PARAMS={'max_depth': 3, 'learning_rate': 0.1, 'n_estimators': 100}

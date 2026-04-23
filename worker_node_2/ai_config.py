643845832640ade102077b16492f7be76c789718
jeandre <jeandre.c127@gmail.com>
2026-04-23 13:46:55 +0200

    Auto-experiment: FEATURES=['cvd_trend', 'atr_14', 'rsi_14', 'macd_l...

--- worker_node_2/ai_config.py
+++ worker_node_2/ai_config.py
@@ -1,4 +1,4 @@
-FEATURES=['cvd_trend', 'rsi_14', 'macd_line']
+FEATURES=['cvd_trend', 'atr_14', 'rsi_14', 'macd_line', 'bb_lower', 'bb_upper']
 TARGET_LOOKAHEAD=2
-THRESHOLD_PERCENTILE=85
-MODEL_PARAMS={'max_depth': 6, 'learning_rate': 0.1, 'n_estimators': 100}
+THRESHOLD_PERCENTILE=72
+MODEL_PARAMS={'max_depth': 5, 'learning_rate': 0.15, 'n_estimators': 120, 'subsample': 0.8, 'colsample_bytree': 0.7, 'min_child_weight': 3}
